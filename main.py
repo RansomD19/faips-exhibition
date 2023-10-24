bb460f18e067e614f47cceea0b3082ee43833a83
RansomD19 <danielgg.george@gmail.com>
2023-10-24 22:11:17 +0300

    Final Update only adding images remaining plus an href styling

--- main.py
+++ main.py
@@ -1,22 +1,32 @@
 from flask import Flask, render_template
+import pandas as pd
 
 #create a falsk instance
+app = Flask(__name__)
+
+#reading csv
+data = pd.read_csv('data2.csv')
+team_names = data['Team Name']
+team_members = data['Team Members']
+descs = data['Project Description']
+img = data['Project Image']
 app = Flask(__name__)
 
 # create a route decorator
 @app.route("/")
 
 def index():
-    first_name = "John Doe"
-    stuff = "This is <strong>Bold</strong>"
-    fav = [1, 2, 3, 4, 4]
-    return render_template("index.html", stuff=stuff, faviii=fav)
+    return render_template("index.html")
 
 @app.route("/content/<id>")
-
 def content(id):
-    return render_template("content.html", id=id)
+    id = int(id)
+    team_name = team_names[id-1]
+    team_member = team_members[id-1]
+    desc = descs[id-1]
+    image = img[id-1]  
 
+    return render_template('content.html', team_name = team_name, team_member = team_member, desc = desc, image=image)
 
 #create a custom error pages
 #invalid URL
