ab4ee49a282eaa3e47ef61b350ca61d361cfc865
RansomD19 <danielgg.george@gmail.com>
2023-10-26 20:14:31 +0300

    Final Push for Production

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from flask import Flask, render_template
 import pandas as pd
 
-#create a falsk instance
+# create a falsk instance
 app = Flask(__name__)
 
-#reading csv
+# reading csv
+# data = pd.read_csv('/home/ransomd19/mysite/data2.csv') # FOR PRODUCTION rememebr to uncomment
 data = pd.read_csv('data2.csv')
+
 team_names = data['Team Name']
 team_members = data['Team Members']
 descs = data['Project Description']
@@ -13,10 +15,12 @@ img = data['Project Image']
 app = Flask(__name__)
 
 # create a route decorator
-@app.route("/")
 
+
+@app.route("/")
 def index():
     return render_template("index.html")
+
 
 @app.route("/content/<id>")
 def content(id):
@@ -24,16 +28,19 @@ def content(id):
     team_name = team_names[id-1]
     team_member = team_members[id-1]
     desc = descs[id-1]
-    image = img[id-1]  
+    image = img[id-1]
+    return render_template('content.html', team_name=team_name, team_member=team_member, desc=desc, image=image)
 
-    return render_template('content.html', team_name = team_name, team_member = team_member, desc = desc, image=image)
+# create a custom error pages
+# invalid URL
 
-#create a custom error pages
-#invalid URL
+
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template("404.html"), 404
-#Internal server error
+# Internal server error
+
+
 @app.errorhandler(500)
 def page_not_found(e):
     return render_template("500.html"), 500
