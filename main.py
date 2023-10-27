5cc0fecb6bd8d1485b6f79f38d4c48c028b89fb3
JazzGulShah <107211680+JazzGulShah@users.noreply.github.com>
2023-10-27 13:45:04 +0300

    Update main.py

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html", num=team_members)
 
 
 @app.route("/content/<id>")
